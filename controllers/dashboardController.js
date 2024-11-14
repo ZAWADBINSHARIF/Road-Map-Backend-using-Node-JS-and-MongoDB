13496168f98b68aed28e794c34def143afff6dff
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2024-11-15 02:31:02 +0600

    User and Admin dashboard screen has been done

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -6,6 +6,7 @@ import asyncHandler from "express-async-handler";
 import User from "../models/User.js";
 import Branch from "../models/branch.js";
 import CaseContainer from "../models/caseContainer.js";
+import AnswerCase from "../models/answerCase.js";
 
 
 
@@ -22,14 +23,20 @@ export const getDashboardInfo = asyncHandler(async (req, res) => {
 
         if (foundUser.rule === "admin") {
 
+            const totalUser = await User.countDocuments({});
+            const list = await AnswerCase.countDocuments({});
+            const totalCaseContainer = await CaseContainer.countDocuments({});
+            const totalSection = await Branch.countDocuments({});
+            console.log({ totalUser, list, totalCaseContainer, totalSection });
+            return res.status(200).json({ totalUser, list, totalCaseContainer, totalSection });
 
         } else {
             const myCases = foundUser?.myCases?.filter(item => item.publish === true).length;
             const myList = foundUser?.myList.length;
-            const totalCases = await CaseContainer.countDocuments({ publish: true });
+            const totalCaseContainer = await CaseContainer.countDocuments({ publish: true });
             const totalSection = await Branch.countDocuments({ publish: true });
-            console.log({ myCases, myList, totalCases, totalSection });
-            return res.status(200).json({ myCases, myList, totalCases, totalSection });
+            console.log({ myCases, myList, totalCaseContainer, totalSection });
+            return res.status(200).json({ myCases, myList, totalCaseContainer, totalSection });
 
         }
 
