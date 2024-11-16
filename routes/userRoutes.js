b5c339175d1f252b98b87db57652c17882d725f9
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2024-11-16 18:11:34 +0600

    [Bug fix]: User profile update bug fixed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,8 +14,8 @@ router.get("/myCases", JWT_verifier("user"), getMyCases);
 router.get("/all_user", JWT_verifier("admin"), getAllUser);
 router.get("/", JWT_verifier(), getUserDetails);
 
-router.put("/", JWT_verifier("user"), userProfileImageUploader, updateUserDetails);
 router.put("/changeRole/:userId", JWT_verifier("admin"), changeUserRole);
+router.put("/", JWT_verifier(), userProfileImageUploader, updateUserDetails);
 
 router.delete("/removeUser/:userId", JWT_verifier("admin"), removingUser);
 
